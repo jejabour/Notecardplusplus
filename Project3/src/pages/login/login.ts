9aca11accd818d09f9d47c047780a1f80d652925
TaylorColeSmith <43576481+TaylorColeSmith@users.noreply.github.com>
2018-10-27 19:48:12 -0500

    Add files via upload

--- Project3/src/pages/login/login.ts
+++ Project3/src/pages/login/login.ts
@@ -1,32 +1,59 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams 
-} from 'ionic-angular';
-import{ HomePage } from '../home/home'
+import { FirebaseProvider } from providers/firebase/firebase;
 
-/**
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
+/*
  * Generated class for the LoginPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
+ import {  AngularFireAuth  } from 'angularfire2/auth';
 
+import firebase from 'firebase';
 @IonicPage()
 @Component({
-
+  selector: 'page-login',
   templateUrl: 'login.html',
 })
 export class LoginPage {
+  google = {
+     loggedin: false,
+     name: '',
+     profilePicture: '',
+     email:''
+  }
+  constructor(private fire: AngularFireAuth, public navCtrl: NavController, public navParams: NavParams) {
+  }
 
-
-  	constructor(public navCtrl: NavController, public navParams: NavParams) {
-  	}
-
- 	ionViewDidLoad() {
-   	 console.log('ionViewDidLoad LoginPage');
-    }
-
-    home(){
-    	this.navCtrl.setRoot(HomePage);
-    }
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+  home(){
+    
+    this.navCtrl.setRoot(HomePage);
+    
+  }
+ 
+  googleSignin(){
   
+      this.fire.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then( res => {
+         console.log('From --Google--');
+         console.log(res);
+         this.loggedin = true;
+         this.google.name = res.user.displayName;
+         this.google.email = res.user.email;
+         this.google.profilePicture = res.user.photoUrl;
+      })
+      this.fire.authState.subscribe(loggedin =>{
+
+        
+         if(loggedin){   
+             this.navCtrl.setRoot(HomePage);
+             }
+      });
+     
+  }
 }
